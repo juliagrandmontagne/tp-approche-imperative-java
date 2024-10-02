cc5e31e2cf3d9cf89fe7387749148d7b9d630c35
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-02 15:05:49 +0200

    secondcommit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,18 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte var1 = 1 ;
+        short var2 = 2 ;
+        int var3 = 3 ;
+        long var4 = 4 ;
+        float var5 = 5 ;
+        double var6 = 6 ;
+        char var7 = 'h';
+        boolean var8 = true ;
+        String var9 = "helloword";
+        System.out.println(var7);
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+        System.out.println(randomString);
+    }
+}
