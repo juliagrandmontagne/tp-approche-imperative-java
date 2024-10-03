bdb62b8b9dff08808a31492b991368a837e419c8
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-03 09:30:55 +0200

    tp deb autonomie

---
+++ src/fr/algorithmie/AffichageIdentite.java
@@ -0,0 +1,11 @@
+package fr.algorithmie;
+
+public class AffichageIdentite {
+    public static void main(String[] args)
+    {
+        for ( int i = 0; i < 10; i++)
+        {
+            System.out.println("JuliaGrandmontagne");
+        }
+    }
+}
---
+++ src/fr/algorithmie/AffichageSuite.java
@@ -0,0 +1,30 @@
+package fr.algorithmie;
+
+public class AffichageSuite {
+    public static void main(String[] args)
+    {
+        //Utiliser une boucle for pour afficher tous les nombres de 1 à 10 inclus
+       /* for ( int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+        //Utiliser une boucle for pour afficher tous les nombres pairs entre 0 et 10 inclus
+        for ( int i = 0; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+        //Utiliser une boucle for pour afficher tous les nombres impairs entre 0 et 9 inclus
+        for ( int i = 1; i <= 10; i = i + 2)
+        {
+            System.out.println(i);
+        }*/
+        //Utiliser une boucle while pour afficher tous les nombres de 1 à 10 inclus
+        int j = 1;
+        while (j <= 10)
+        {
+            System.out.println(j);
+            j++;
+        }
+
+    }
+}
