27166b1f56c38f536f9ec664a4326e22eec1c595
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-03 16:53:20 +0200

    tp 5 i need a breack

---
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -0,0 +1,11 @@
+package fr.boucles;
+
+public class ExerciceTableauEntier {
+    public static void main(String[] args)  {
+        int[]array={1,2,3,4,5,6,7,8,9,10};
+        System.out.println(array[0]);
+        System.out.println(array.length);
+        System.out.println(array.length);
+        array[4] = 8;
+    }
+}
