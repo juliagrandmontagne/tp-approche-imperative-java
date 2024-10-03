9a9b0eacb906d4c22717be3952428edf4d4f83f8
juliagrandmontagne <85647414+juliagrandmontagne@users.noreply.github.com>
2024-10-03 18:07:54 +0200

    tp 5

--- src/fr/boucles/ExerciceBouclesEtTests.java
+++ src/fr/boucles/ExerciceBouclesEtTests.java
@@ -11,5 +11,37 @@ public class ExerciceBouclesEtTests {
     {
         System.out.println(array[i]);
     }
+        for ( int i = 0; i < array.length; i++)
+        {
+            if (array[i]>3)
+            {
+                System.out.println(array[i]);
+            }
+            else { }
+        }
+        for ( int i = 0; i < array.length; i++)
+        {
+            if (array[i] % 2 == 0 )
+            {
+                System.out.println(array[i]);
+            }
+        }
+        //Combiner une boucle et un test de manière à n’afficher que les valeurs correspondant aux index pairs
+        for ( int i = 0; i < array.length; i++)
+        {
+            if (i % 2 == 0 )
+            {
+                System.out.println(array[i]);
+            }
+        }
+        //Combiner une boucle et un test de manière à n’afficher que les entiers impairs
+        for ( int i = 0; i < array.length; i++)
+        {
+            if (array[i] % 2 == 1 )
+            {
+                System.out.println(array[i]);
+            }
+        }
     }
+
 }
